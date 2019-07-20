2646c67cec7306fe1d97ed0716d9d9b2dd9f1be9
andreastkdf <andreaskostakis@gmail.com>
2019-07-20 14:53:17 +0100

    AddEntry - AtkdfSteppers

--- components/AtkdfSteppers.js
+++ components/AtkdfSteppers.js
@@ -1,10 +1,31 @@
 import React from "react"
-import { View, Text } from "react-native"
+import { View, Text, TouchableOpacity } from "react-native"
+import { FontAwesome, Entypo } from "@expo/vector-icons"
 
-export default function AtkdfSteppers() {
+const AtkdfSteppers = ({
+  max,
+  unit,
+  value,
+  step,
+  onIncrement,
+  onDecrement
+}) => {
   return (
     <View>
-      <Text>AtkdfSteppers</Text>
+      <View>
+        <TouchableOpacity onPress={onDecrement}>
+          <FontAwesome name="minus" size={30} color={"black"} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onIncrement}>
+          <FontAwesome name="plus" size={30} color={"black"} />
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
     </View>
   )
 }
+
+export default AtkdfSteppers
