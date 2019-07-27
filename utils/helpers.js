efb396c2f53caa0f774d4a323630b1af5aa60641
andreastkdf <andreaskostakis@gmail.com>
2019-07-27 16:57:21 +0100

    Changes for style-metainfo

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,29 @@
 import React from "react"
-import { View } from "react-native"
+import { View, StyleSheet } from "react-native"
 import {
   FontAwesome,
   MaterialIcons,
   MaterialCommunityIcons
 } from "@expo/vector-icons"
-import { white } from "./colors"
+import { white, red, orange, blue, lightPurp, pink } from "./colors"
+
+export const getDailyReminderValue = () => {
+  return {
+    today: "👋🏻 Don't forget to log your data today!"
+  }
+}
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    padding: 5,
+    borderRadius: 8,
+    width: 50,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 20
+  }
+})
 
 export function isBetween(num, x, y) {
   if (num >= x && num <= y) {
@@ -61,8 +79,8 @@ export function getMetricMetaInfo(metric) {
       type: "steppers",
       getIcon() {
         return (
-          <View>
-            <MaterialIcons name="directions-run" color={"black"} size={35} />
+          <View style={[styles.iconContainer, { backgroundColor: red }]}>
+            <MaterialIcons name="directions-run" color={"white"} size={35} />
           </View>
         )
       }
@@ -75,8 +93,8 @@ export function getMetricMetaInfo(metric) {
       type: "steppers",
       getIcon() {
         return (
-          <View>
-            <MaterialCommunityIcons name="bike" color={"black"} size={35} />
+          <View style={[styles.iconContainer, { backgroundColor: orange }]}>
+            <MaterialCommunityIcons name="bike" color={"white"} size={35} />
           </View>
         )
       }
@@ -89,8 +107,8 @@ export function getMetricMetaInfo(metric) {
       type: "steppers",
       getIcon() {
         return (
-          <View>
-            <MaterialCommunityIcons name="swim" color={"black"} size={35} />
+          <View style={[styles.iconContainer, { backgroundColor: blue }]}>
+            <MaterialCommunityIcons name="swim" color={"white"} size={35} />
           </View>
         )
       }
@@ -103,8 +121,8 @@ export function getMetricMetaInfo(metric) {
       type: "slider",
       getIcon() {
         return (
-          <View>
-            <FontAwesome name="bed" color={"black"} size={35} />
+          <View style={[styles.iconContainer, { backgroundColor: lightPurp }]}>
+            <FontAwesome name="bed" color={"white"} size={35} />
           </View>
         )
       }
@@ -117,8 +135,8 @@ export function getMetricMetaInfo(metric) {
       type: "slider",
       getIcon() {
         return (
-          <View>
-            <MaterialCommunityIcons name="food" color={"black"} size={35} />
+          <View style={[styles.iconContainer, { backgroundColor: pink }]}>
+            <MaterialCommunityIcons name="food" color={"white"} size={35} />
           </View>
         )
       }
@@ -126,10 +144,4 @@ export function getMetricMetaInfo(metric) {
   }
 
   return typeof metric === "undefined" ? info : info[metric]
-}
-
-export const getDailyReminderValue = () => {
-  return {
-    today: "👋🏻 Don't forget to log your data today!"
-  }
 }
